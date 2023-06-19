a089056732d2e758621836b8f136b2eb361419c0
minda-li <122487902+minda-li@users.noreply.github.com>
2023-06-19 06:52:09 -0700

    Add files via upload

--- query_writer.py
+++ query_writer.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import os
 
 csv_file = "leetcode_dataset - lc.csv"
 
@@ -12,9 +13,24 @@ with open(csv_file, "r") as file:
         difficulty = row[4]
         question = row[2]
 
-        if difficulty.lower() == "hard":
+        if difficulty.lower() == "medium":
             file_name = f"query{question_number}.txt"
-            file_content = f"Write a python program to do the following, and put this program in a json object as the value corresponding to the key 'code'.\n{question}"
+            extracted_file_name = f"extracted{question_number}.txt"
+
+            # Check if the extracted file exists
+            if not os.path.exists(extracted_file_name):
+                print(f"Skipped {question_number} as the extracted file doesn't exist")
+                continue
+
+            # Read the content to insert from the extracted file
+            with open(extracted_file_name, "r") as extracted_file:
+                extracted_content = extracted_file.read()
+
+            # Construct the file content with the extracted content
+            file_content = f"""Write a python program to do the following, and put this program in a json object as the value corresponding to the key 'code'.
+{question}
+Write code that fits the following template: The program should continue below the following lines:
+{extracted_content}"""
 
             with open(file_name, "w") as output_file:
                 output_file.write(file_content)
