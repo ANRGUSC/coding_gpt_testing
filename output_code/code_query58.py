9406ca46ecb20bee961deb044fda21cbfd63d014
minda-li <122487902+minda-li@users.noreply.github.com>
2024-11-12 12:53:36 -0800

    Delete output_code directory

--- output_code/code_query58.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        words = s.split()
-        if len(words) == 0:
-            return 0
-        else:
-            return len(words[-1])
